1c3aa6e51738c4f5d35ac4eeb871884e4f3ea4a3
ayush-mundra <63422678+ayush-mundra@users.noreply.github.com>
2021-06-20 12:34:42 +0530

    changes done in views.py

--- loanpredictor/views.py
+++ loanpredictor/views.py
@@ -9,6 +9,7 @@ def home(request):
 	return render(request, "home.html")
 
 def result(request):
+<<<<<<< Updated upstream
 	cls = joblib.load("finalized_model.sav")
 	normalize = joblib.load("normalize.sav")
 	lis = []
@@ -39,3 +40,20 @@ def result(request):
 	ans = cls.predict([lis])
 	return render(request, "result.html", {"ans":ans})
 
+=======
+	ApplicantIncome = float(request.POST.get("ApplicantIncome"))
+	CoapplicantIncome = float(request.POST.get("CoapplicantIncome"))
+	LoanAmount = float(request.POST.get("LoanAmount"))
+	Loan_Amount_Term = float(request.POST.get("Loan_Amount_Term"))
+	Credit_History = float(request.POST.get("Credit_History"))
+	Dependents = float(request.POST.get("Dependents"))
+	Male = float(request.POST.get("Male"))
+	married_Yes = float(request.POST.get("married_Yes"))
+	Not_graduate = float(request.POST.get("Not_graduate"))
+	Self_Employed = float(request.POST.get("Self_Employed"))
+	Rural = float(request.POST.get("Rural"))
+	SemiUrban = float(request.POST.get("SemiUrban"))
+	Urban = float(request.POST.get("Urban"))
+	result = getPredictions(ApplicantIncome, CoapplicantIncome, Loan_Amount_Term,  LoanAmount, Credit_History, Dependents, Male, married_Yes, Not_graduate, Self_Employed, Rural, SemiUrban, Urban)
+	return render(request, "result.html", {"ans": result})
+>>>>>>> Stashed changes
